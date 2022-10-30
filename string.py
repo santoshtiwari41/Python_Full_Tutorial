befb6276695bae24bec9e11dcc0e2f6564d0989c
santoshtiwari41 <santoshhh066@gmail.com>
2022-10-30 15:03:59 +0545

    initial commit

---
+++ string.py
@@ -0,0 +1,11 @@
+print("this is python tutorial");
+    # creating string in python
+print("string with single quotes:");
+x='hello';
+print(x);
+print("string with double quotes:");
+a="this is string";
+print(a);
+print("string with triple quotes:")
+b='''this is string with "triple" quotes:''';
+print(b);
